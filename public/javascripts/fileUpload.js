b0b7aa8400f39ac9019e9d97903366c53498136a
jerrytigerxu <jere.tiger.xu@gmailc.om>
2022-09-30 15:30:18 -0400

    filepond stuff

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -3,4 +3,11 @@ FilePond.registerPlugin(
     FilePondPluginImageResize,
     FilePondPluginFileEncode,
 );
+
+FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+})
+
 FilePond.parse(document.body);
